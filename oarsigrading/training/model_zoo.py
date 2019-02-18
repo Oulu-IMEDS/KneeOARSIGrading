KL grading seems to work. There was a bug in OAI data preparation script

oarsigrading/training/model_zoo.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import pretrainedmodels
-
+from termcolor import colored
 
 class ViewerFC(nn.Module):
     def forward(self, x):
@@ -12,14 +12,19 @@ class SeResNet(nn.Module):
         super(SeResNet, self).__init__()
         if layers == 18:
             model = pretrainedmodels.__dict__['resnet18'](num_classes=1000, pretrained='imagenet')
+            print(colored('====> ', 'green') + 'Pre-trained resnet18 is used as backbone')
         elif layers == 34:
             model = pretrainedmodels.__dict__['resnet34'](num_classes=1000, pretrained='imagenet')
+            print(colored('====> ', 'green') + 'Pre-trained resnet34 is used as backbone')
         elif layers == 50:
             model = pretrainedmodels.__dict__['se_resnet50'](num_classes=1000, pretrained='imagenet')
+            print(colored('====> ', 'green') + 'Pre-trained se-resnet50 is used as backbone')
         elif layers == 101:
             model = pretrainedmodels.__dict__['se_resnet101'](num_classes=1000, pretrained='imagenet')
+            print(colored('====> ', 'green') + 'Pre-trained se-resnet101 is used as backbone')
         elif layers == 152:
             model = pretrainedmodels.__dict__['se_resnet152'](num_classes=1000, pretrained='imagenet')
+            print(colored('====> ', 'green') + 'Pre-trained se-resnet152 is used as backbone')
         else:
             raise NotImplementedError
 

oarsigrading/training/utils.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 
 from oarsigrading.kvs import GlobalKVS
 from oarsigrading.training.model import MultiTaskClassificationLoss, OARSIGradingNet
+from oarsigrading.training.model_zoo import SeResNet
 
 
 def net_core(net: nn.Module) -> nn.Module:
@@ -29,14 +30,16 @@ def layer_params(net: nn.Module, layer_name: str):
 
 
 def init_loss() -> nn.Module:
-    return MultiTaskClassificationLoss()
+    return nn.CrossEntropyLoss()  # MultiTaskClassificationLoss()
 
 
 def init_model() -> Tuple[nn.Module, nn.Module]:
     kvs = GlobalKVS()
 
-    net = OARSIGradingNet(bb_width=kvs['args'].backbone_width, dropout=kvs['args'].dropout_rate,
-                          cls_bnorm=kvs['args'].use_bnorm)
+    # net = OARSIGradingNet(bb_width=kvs['args'].backbone_width, dropout=kvs['args'].dropout_rate,
+    #                       cls_bnorm=kvs['args'].use_bnorm)
+
+    net = SeResNet(kvs['args'].backbone_width, kvs['args'].dropout_rate, 5)
 
     if kvs['gpus'] > 1:
         net = nn.DataParallel(net).to('cuda')
@@ -84,7 +87,7 @@ def epoch_pass(net: nn.Module, loader: DataLoader, criterion: nn.Module,
                 optimizer.zero_grad()
 
             # forward + backward + optimize
-            labels = batch['target'].squeeze().to(device)
+            labels = batch['target'].squeeze()[:, 0].to(device)
             inputs = batch['img'].squeeze().to(device)
 
             outputs = net(inputs)