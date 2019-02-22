ef979aaa9806fee11d8c38ab4a82bac4982e5378
lext <aleksei.tiulpin@protonmail.ch>
2019-02-23 00:11:53 +0200

    better GWAP experiments

--- run_experiments_gwap.sh
+++ run_experiments_gwap.sh
@@ -7,12 +7,40 @@ SNP_PREF=2019_02
 
 cd scripts
 
+# GWAP
 
 #python train.py --backbone_depth 18 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
 # --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True
 
-#python train.py --backbone_depth 18 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
-# --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap_hidden True --use_gwap
+python train.py --backbone_depth 34 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --se True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --se True --dw True
+
+# GWAP hidden used
+
+python train.py --backbone_depth 18 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --use_gwap_hidden True
+
+python train.py --backbone_depth 34 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --use_gwap_hidden True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --use_gwap_hidden True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --se True --use_gwap_hidden True
+
+python train.py --backbone_depth 50 --weighted_sampling True --snapshots ${SNAPSHOTS_DIR} \
+ --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --use_gwap True --se True --dw True --use_gwap_hidden True
+
 
 
 for SNP_NAME in $(ls ${SNAPSHOTS_DIR} | grep ${SNP_PREF});
