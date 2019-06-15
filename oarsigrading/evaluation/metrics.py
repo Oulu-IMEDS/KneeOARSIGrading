d630deff3c60eedd1704d4a5f5d38acc0cb552e5
lext <aleksei.tiulpin@protonmail.ch>
2019-06-15 09:31:10 +0300

    Added no_kl option (untested)

--- oarsigrading/evaluation/metrics.py
+++ oarsigrading/evaluation/metrics.py
@@ -4,19 +4,28 @@ import os
 from oarsigrading.kvs import GlobalKVS
 
 
-def compute_metrics(val_pred, val_gt):
+def compute_metrics(val_pred, val_gt, no_kl=False):
     kappas = []
     for task_id in range(val_gt.shape[1]):
         kappas.append(cohen_kappa_score(val_gt[:, task_id], val_pred[:, task_id], weights='quadratic'))
 
-    kappa_res = {'kappa_kl': kappas[0],
-                 'kappa_ostl': kappas[1],
-                 'kappa_osfl': kappas[2],
-                 'kappa_jsl': kappas[3],
-                 'kappa_ostm': kappas[4],
-                 'kappa_osfm': kappas[5],
-                 'kappa_jsm': kappas[6]
-                 }
+    if no_kl:
+        kappa_res = {'kappa_ostl': kappas[0],
+                     'kappa_osfl': kappas[1],
+                     'kappa_jsl': kappas[2],
+                     'kappa_ostm': kappas[3],
+                     'kappa_osfm': kappas[4],
+                     'kappa_jsm': kappas[5]
+                     }
+    else:
+        kappa_res = {'kappa_kl': kappas[0],
+                     'kappa_ostl': kappas[1],
+                     'kappa_osfl': kappas[2],
+                     'kappa_jsl': kappas[3],
+                     'kappa_ostm': kappas[4],
+                     'kappa_osfm': kappas[5],
+                     'kappa_jsm': kappas[6]
+                     }
 
     print(colored('==> ', 'green') + f' Kappas:')
     for k in kappa_res:
@@ -33,7 +42,7 @@ def log_metrics(boardlogger, train_loss, val_loss, val_pred, val_gt):
     }
     print(colored('==> ', 'green') + f'Train loss: {train_loss:.4f} / Val loss: {val_loss:.4f}')
 
-    res.update(compute_metrics(val_pred, val_gt))
+    res.update(compute_metrics(val_pred, val_gt, no_kl=kvs['args'].no_kl))
 
     boardlogger.add_scalars('Losses', {'train': train_loss, 'val': val_loss}, kvs['cur_epoch'])
     boardlogger.add_scalars('Metrics', {metric: res[metric] for metric in res if metric.startswith('kappa')},
