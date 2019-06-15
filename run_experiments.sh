04bb3219dbc0cc9fda80c94a86987e53c9375dec
lext <aleksei.tiulpin@protonmail.ch>
2019-06-15 09:34:29 +0300

    commented exp

--- run_experiments.sh
+++ run_experiments.sh
@@ -26,8 +26,8 @@ cd scripts
 #python train.py --backbone_depth 50 --se True --dw True --snapshots ${SNAPSHOTS_DIR} \
 # --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --pretrained True
 
-python train.py --backbone_depth 50 --se True --dw True --snapshots ${SNAPSHOTS_DIR} \
- --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --lr_drop 10 15
+#python train.py --backbone_depth 50 --se True --dw True --snapshots ${SNAPSHOTS_DIR} \
+# --dataset_root ${DATA_DIR} --meta_root ${META_DIR} --lr_drop 10 15 --lr 0.0001
 
 # -------------------------------------------------------------- #
 # --------------------KL-based Weighing------------------------- #
